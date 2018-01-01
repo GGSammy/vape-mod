c2be6c6f1924976ad56946ca22e54b1e1b5ca282
Collin <collinm1@verizon.net>
2018-01-01 16:29:33 -0500

    Added TestVape to tools tab

--- src/main/java/com/weebly/rneptune/ItemVape.java
+++ src/main/java/com/weebly/rneptune/ItemVape.java
@@ -1,10 +1,12 @@
 package com.weebly.rneptune;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class ItemVape extends Item {
 	public ItemVape() {
 		setUnlocalizedName(Main.VapeItems.VAPE.getUnlocalizedName());
 		setRegistryName(Main.VapeItems.VAPE.getRegistryName());
+        this.setCreativeTab(CreativeTabs.TOOLS);
 	}
 }
